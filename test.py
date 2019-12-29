acf41e0cd8944f7a5bddd7b594a5aa411c5d3b31
Jessica Yang <jyang@dgx-server.lnx.boozallencsn.com>
2019-12-29 18:47:10 -0500

    Performance compare CPU with GPU

--- test.py
+++ test.py
@@ -11,8 +11,8 @@ def test(network, test_losses, test_loader):
             test_loss += F.nll_loss(output, target, size_average=False).item()
             pred = output.data.max(1, keepdim=True)[1]
             correct += pred.eq(target.data.view_as(pred)).sum()
-    test_loss /= len(test_loader.dataset)
+    test_loss /= len(test_loader.dl.dataset)
     test_losses.append(test_loss)
     print('\nTest set: Avg. loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
-        test_loss, correct, len(test_loader.dataset),
-        100. * correct / len(test_loader.dataset)))
+        test_loss, correct, len(test_loader.dl.dataset),
+        100. * correct / len(test_loader.dl.dataset)))
